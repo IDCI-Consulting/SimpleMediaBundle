45f31842de3ae0b61aed2e4511cbcc86f5d5b05d
Pierre FERROLLIET <pierre.ferrolliet@idci-consulting.fr>
2013-01-22 16:46:19 +0100

    Works on media manager

--- Repository/MediaOwnerMediaRepository.php
+++ Repository/MediaOwnerMediaRepository.php
@@ -9,7 +9,44 @@
 
 namespace IDCI\Bundle\SimpleMediaBundle\Repository;
 
-class MediaOwnerMediaRepository {
+class MediaOwnerMediaRepository
+{
+    /**
+     * Find medias for a given hash 
+     * 
+     * @return entities
+     */
+    public function findfindMediasByHash($hash)
+    {
+        $q = $this->findfindMediasByHashQuery();
 
+        return is_null($q) ? array() : $q->getResult();
+    }
+    
+    /**
+     * Find medias for a given hash  based on a query
+     *
+     * @return DoctrineQuery
+     */
+    public function findfindMediasByHashQuery($hash)
+    {
+        $qb = $this->findMediasByHashQueryBuilder($hash);
+
+        return is_null($qb) ? $qb : $qb->getQuery();
+    }
+    
+    /**
+     * Find medias for a given hash based on a query builder
+     *
+     * @return DoctrineQueryBuilder
+     */
+    public function findMediasByHashQueryBuilder($hash)
+    {
+        $qb = $this->createQueryBuilder("m")
+                ->where("m.hash = :hash")
+                ->setParameter("hash", $hash);
+
+        return $qb;
+    }
 }
 ?>
--- Service/Manager.php
+++ Service/Manager.php
@@ -7,9 +7,34 @@
  *
  */
 
+use IDCI\Bundle\SimpleMediaBundle\Entity\Media;
+use IDCI\Bundle\SimpleMediaBundle\Entity\MediaOwnerMedia;
+use Doctrine\ORM\EntityManager;
+
 namespace IDCI\Bundle\SimpleMediaBundle\Service;
 
 class Manager
 {
-
+    protected $em;
+    
+    public function __construct(EntityManager $em)
+    {
+        $this->em = $em;
+    }
+    
+    public function addMedia($hash, Media $media, array $tags = array())
+    {
+        $mom = new MediaOwnerMedia();
+        $mom->setMedia($media);
+        $mom->setHash($hash);
+        $mom->setTags($tags);
+        
+        $this->em->persist($mom);
+        $this->em->flush();
+    }
+    
+    public function getMedias($hash)
+    {
+        return $this->em->getRepository('IDCISimpleMediaBundle:MediaOwnerMedia')->findMediasByHash($hash);
+    }
 }
